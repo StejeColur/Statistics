18699b5cdde1c717578a2af926d039ad4361d539
Stephan Jensen <sj@zund.dk>
2019-06-18 08:10:12 +0200

    Git Add'ed one folder to deep

--- app/App.js
+++ app/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import StatApp from './src/App';
+import { Provider } from 'react-redux';
+
+import { confStore } from './src/_helpers';
+const store = confStore();
 
 export default class App extends React.Component {
-  render() {
-      return <StatApp />;
-  }
+    render() {
+        return(
+            <Provider store={store}>
+                <StatApp />
+            </Provider>
+        );
+    }
 }
 
